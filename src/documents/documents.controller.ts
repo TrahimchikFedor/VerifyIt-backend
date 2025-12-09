f9ef12d29388aecb86b3bf24208ae402f53a9d2c
TrahimchikFedor <trahimchik.fedor@gmail.com>
2025-12-09 17:18:58 +0300

    add endpoint verifyDocument

--- src/documents/documents.controller.ts
+++ src/documents/documents.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { DocumentsService } from './documents.service';
+import { Authorization } from 'src/auth/decorators/authorization.decorator';
 
 @Controller('documents')
 export class DocumentsController {
   constructor(private readonly documentsService: DocumentsService) {}
+
+  @Authorization()
+  @Get('/:id')
+  async verifyDocument(@Param('id') id: string){
+    return await this.documentsService.verifyDocument(id);
+  }
+
 }
--- src/documents/documents.service.ts
+++ src/documents/documents.service.ts
@@ -2,24 +2,60 @@ import { BadRequestException, Injectable, NotFoundException } from '@nestjs/comm
 import { Document } from 'prisma/generated/prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { DocumentResponseDto } from './dto/document-response.dto';
+import { AllLogger } from 'src/common/log/logger.log';
 
 @Injectable()
 export class DocumentsService {
 
+    private readonly name = DocumentsService.name;
+    private readonly logger = new AllLogger();
     constructor(
         private prismaService: PrismaService
     ){}
 
 
     async verifyDocument(id: string): Promise<DocumentResponseDto> {
+        this.logger.log("Try to verify document", this.name)
         const document = await this.prismaService.document.findUnique({
             where: { id }
         });
 
         if (!document) {
+            this.logger.warn("Документ не найден", this.name);
             throw new NotFoundException("Документ не найден");
         }
 
+        const response = this.getDocumentInfo(document);
+
+        this.logger.log("Successful", this.name)
+        return response;
+    }
+
+    async getHistory(id: string){
+        this.logger.log("Try get history", this.name)
+        const history = await this.prismaService.history.findMany({
+            where:{
+                userId: id
+            },
+            orderBy:{
+                createdAt: 'desc'
+            },
+            select:{
+                document: true
+            }
+        });
+
+        if(!history.length){
+            this.logger.warn("История пуста", this.name);
+            throw new NotFoundException("История пуста");
+        }
+
+        history.map(entry => this.getDocumentInfo(entry.document));
+
+    }
+
+    private getDocumentInfo(document: Document):DocumentResponseDto{
+        
         const now = new Date();
         const expiration = new Date(document.expirationDate);
 
@@ -35,17 +71,5 @@ export class DocumentsService {
         };
 
         return response;
-    }
-
-    async getHistory(id: string){
-        const history = await this.prismaService.history.findMany({
-            where:{
-                userId: id
-            },
-            orderBy:{
-                createdAt: 'desc'
-            }
-        });
-
     }
 }
