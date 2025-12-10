dfd2407374c22b19777899f1538cf05c20c8ec91
TrahimchikFedor <trahimchik.fedor@gmail.com>
2025-12-10 06:31:50 +0300

    add JwtService to providers

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [MessagesController],
-  providers: [MessagesService],
+  providers: [MessagesService, JwtService],
   exports: [MessagesService]
 })
 export class MessagesModule {}
