1f021e386b233f19c946947b69fd810cf4ea4cb2
Prof-Woland <sorokonikita08@gmail.com>
2025-12-10 04:35:23 +0300

    messages user fix

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -3,6 +3,8 @@ import { Observable, map } from 'rxjs';
 import { MessagesService } from './messages.service';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
 import { Authorization } from 'src/auth/decorators/authorization.decorator';
+import { Authorized } from 'src/auth/decorators/authorized.decorator';
+import { User } from 'prisma/generated/prisma/client';
 
 @Controller('notifications')
 export class MessagesController {
@@ -10,8 +12,8 @@ export class MessagesController {
 
   @Authorization()
   @Sse('stream')
-  stream(@Req() req): Observable<MessageEvent> {
-    const userId = req.user.id;
+  stream(@Authorized() user: User): Observable<MessageEvent> {
+    const userId = user.id;
     return this.messagesService.getUserNotifications(userId).pipe(
       map((notification) => ({
         data: notification,
